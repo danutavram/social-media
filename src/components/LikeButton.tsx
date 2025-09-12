309682774ac28499e5f862054f776de4e670cdea
danutavram <avramnicolaedan@gmail.com>
2025-09-12 19:03:40 +0300

    LikeButton instant vote

--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -1,10 +1,17 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { supabase } from "../supabase-client";
 import { PostDetail } from "./PostDetail";
 import { useAuth } from "../context/AuthContext";
 
 interface Props {
   postId: number;
+}
+
+interface Vote {
+  id: number;
+  post_id: number;
+  user_id: string;
+  vote: number;
 }
 
 const vote = async (voteValue: number, postId: number, userId: string) => {
@@ -30,31 +37,65 @@ const vote = async (voteValue: number, postId: number, userId: string) => {
         .update({ vote: voteValue })
         .eq("id", existingVote.id);
 
-      if(error) throw new Error(error.message);
+      if (error) throw new Error(error.message);
     }
   } else {
-      const { error } = await supabase
-        .from("votes")
-        .insert({ post_id: postId, user_id: userId, vote: voteValue });
-      if (error) throw new Error(error.message);
+    const { error } = await supabase
+      .from("votes")
+      .insert({ post_id: postId, user_id: userId, vote: voteValue });
+    if (error) throw new Error(error.message);
   }
+};
 
+const fetchVotes = async (postId: number): Promise<Vote[]> => {
+  const { data, error } = await supabase
+    .from("votes")
+    .select("*")
+    .eq("post_id", postId);
+
+  if (error) throw new Error(error.message);
+  return data as Vote[];
 };
 
 export const LikeButton = ({ postId }: Props) => {
   const { user } = useAuth();
+
+  const queryClient = useQueryClient()
+
+  const {
+    data: votes,
+    isLoading,
+    error,
+  } = useQuery<Vote[], Error>({
+    queryKey: ["votes", postId],
+    queryFn: () => fetchVotes(postId),
+    refetchInterval: 5000,
+  });
 
   const { mutate } = useMutation({
     mutationFn: (voteValue: number) => {
       if (!user) throw new Error("You must be logged in to Vote!");
       return vote(voteValue, postId, user.id);
     },
+
+    onSuccess: ()=>{
+        queryClient.invalidateQueries({queryKey: ["votes", postId]})
+    }
   });
+
+  if(isLoading) <div> Loading posts...</div>;
+
+  if(error) {
+    return <div> Error: {error.message}</div>
+  }
+
+  const likes = votes?.filter((v) => v.vote === 1).length || 0
+  const dislikes = votes?.filter((v) => v.vote === -1).length || 0
 
   return (
     <div>
-      <button onClick={() => mutate(1)}>👍</button>
-      <button onClick={() => mutate(-1)}>👎</button>
+      <button onClick={() => mutate(1)}>👍 {likes}</button>
+      <button onClick={() => mutate(-1)}>👎 {dislikes}</button>
     </div>
   );
 };
