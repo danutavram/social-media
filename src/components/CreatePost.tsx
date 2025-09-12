f7b52158b18920066c975fdf4a167484ac28237e
danutavram <avramnicolaedan@gmail.com>
2025-09-12 17:46:16 +0300

    Post updated

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -1,10 +1,12 @@
 import { useState, type ChangeEvent } from "react";
 import { useMutation } from "@tanstack/react-query";
 import { supabase } from "../supabase-client";
+import { useAuth } from "../context/AuthContext";
 
 interface PostInput {
   title: string;
   content: string;
+  avatar_url: string | null;
 }
 
 const createPost = async (post: PostInput, imageFile: File) => {
@@ -33,6 +35,7 @@ export const CreatePost = () => {
   const [title, setTitle] = useState<string>("");
   const [content, setContent] = useState<string>("");
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const { user } = useAuth();
 
   const { mutate, isPending, isError } = useMutation({
     mutationFn: (data: { post: PostInput; imageFile: File }) => {
@@ -43,7 +46,14 @@ export const CreatePost = () => {
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
     if (!selectedFile) return;
-    mutate({ post: { title, content }, imageFile: selectedFile });
+    mutate({
+      post: {
+        title,
+        content,
+        avatar_url: user?.user_metadata.avatar_url || null,
+      },
+      imageFile: selectedFile,
+    });
   };
 
   const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -55,40 +65,55 @@ export const CreatePost = () => {
   return (
     <form onSubmit={handleSubmit} className="max-w-2xl mx-auto space-y-4">
       <div>
-          <label htmlFor="title" className="block mb-2 font-medium"> Title</label>
-          <input
-            className="w-full border border-white/10 bg-transparent p-2 rounded"
-            type="text"
-            id="title"
-            required
-            onChange={(event) => setTitle(event.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="content" className="block mb-2 font-medium"> Content</label>
-          <textarea
-            className="w-full border border-white/10 bg-transparent p-2 rounded"
-            id="content"
-            required
-            rows={5}
-            onChange={(event) => setContent(event.target.value)}
-          />
-        </div>
+        <label htmlFor="title" className="block mb-2 font-medium">
+          {" "}
+          Title
+        </label>
+        <input
+          className="w-full border border-white/10 bg-transparent p-2 rounded"
+          type="text"
+          id="title"
+          required
+          onChange={(event) => setTitle(event.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="content" className="block mb-2 font-medium">
+          {" "}
+          Content
+        </label>
+        <textarea
+          className="w-full border border-white/10 bg-transparent p-2 rounded"
+          id="content"
+          required
+          rows={5}
+          onChange={(event) => setContent(event.target.value)}
+        />
+      </div>
 
-        <div>
-          <label htmlFor="image" className="block mb-2 font-medium"> Upload Image</label>
-          <input
-            className="w-full text-gray-200"
-            type="file"
-            id="image"
-            required
-            accept="image/*"
-            onChange={handleFileChange}
-          />
-        </div>
-        <button type="submit" className="bg-purple-500 text-white px-4 py-2 rounded cursor-pointer"> { isPending ? "Creating..." : "Create Post"}</button>
+      <div>
+        <label htmlFor="image" className="block mb-2 font-medium">
+          {" "}
+          Upload Image
+        </label>
+        <input
+          className="w-full text-gray-200"
+          type="file"
+          id="image"
+          required
+          accept="image/*"
+          onChange={handleFileChange}
+        />
+      </div>
+      <button
+        type="submit"
+        className="bg-purple-500 text-white px-4 py-2 rounded cursor-pointer"
+      >
+        {" "}
+        {isPending ? "Creating..." : "Create Post"}
+      </button>
 
-        {isError && <p className="text-red-500"> Error creating post.</p>}
+      {isError && <p className="text-red-500"> Error creating post.</p>}
     </form>
   );
 };
---
+++ src/components/LikeButton.tsx
@@ -0,0 +1,33 @@
+import { useMutation } from "@tanstack/react-query";
+import { supabase } from "../supabase-client";
+import { PostDetail } from "./PostDetail";
+import { useAuth } from "../context/AuthContext";
+
+interface Props {
+  postId: number;
+}
+
+const vote = async (voteValue: number, postId: number, userId: string) => {
+  const {error} = await supabase
+    .from("votes")
+    .insert({ post_id: postId, user_id: userId, vote: voteValue });
+    if(error) throw new Error(error.message)
+};
+
+export const LikeButton = ({ postId }: Props) => {
+  const { user } = useAuth();
+
+  const { mutate } = useMutation({
+    mutationFn: (voteValue: number) => {
+      if (!user) throw new Error("You must be logged in to Vote!");
+      return vote(voteValue, postId, user.id);
+    },
+  });
+
+  return (
+    <div>
+      <button onClick={() => mutate(1)}>👍</button>
+      <button onClick={() => mutate(-1)}>👎</button>
+    </div>
+  );
+};
